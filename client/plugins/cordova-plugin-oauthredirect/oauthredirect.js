a4407846f9a89c00b9065ee70e49ecd85ffc459c
Raymond Cheng <ryscheng@cs.washington.edu>
2015-06-01 10:35:06 -0700

    adding shell plugin

---
+++ client/plugins/cordova-plugin-oauthredirect/oauthredirect.js
@@ -0,0 +1,10 @@
+
+window.echo = function(str, callback) {
+  cordova.exec(callback, function(err) {
+    callback('Nothing to echo.');
+  }, "OAuthRedirect", "echo", [str]);
+};
+
+window.echo("echome", function(echoValue) {
+  alert(echoValue == "echome"); // should alert true.
+});
