ad1458592b7aec5c18ad5eaf84a51f879bf8d38f
Raymond Cheng <ryscheng@cs.washington.edu>
2015-07-15 11:33:04 -0700

    making some names more clear

--- client/plugin-src/cordova-plugin-oauthredirect/src/android/WebServer.java
+++ client/plugin-src/cordova-plugin-oauthredirect/src/android/WebServer.java
@@ -27,25 +27,25 @@ public class WebServer extends NanoHTTPD {
   @Override
   public Response serve(IHTTPSession session) {
     String html = "<html><head>" +
-              // "<meta http-equiv='refresh' content='0;url=" + redirectUrl + "'>" + 
+              // "<meta http-equiv='refresh' content='0;url=" + path + "'>" + 
               "</head><body>" + 
               "<br>" + 
               "<a id='redirectButton'>Return to Colony App</a>" +
               "</body><script>" +
-              "var redirectUrl = '" + session.getUri() + "' + '?access_token=' + window.location.hash.match('access_token=(\\[^&]+)')[1];" + 
-              "console.log(redirectUrl);" + 
+              "var path = '" + session.getUri() + "' + '?access_token=' + window.location.hash.match('access_token=(\\[^&]+)')[1];" + 
+              "console.log(path);" + 
               "var doRedirect = function() {" +
               "  var xhr = new XMLHttpRequest();" +
-              "  xhr.onload = function() { window.location.href = 'app://org.uproxy.colony' + redirectUrl; };" +
-              "  xhr.open('GET', redirectUrl);" +
+              "  xhr.onload = function() { window.location.href = 'app://org.uproxy.colony' + path; };" +
+              "  xhr.open('GET', path);" +
               "  xhr.send();" +
               "};" +
               "doRedirect();" +
-              "document.getElementById('redirectButton').setAttribute('href', 'app://org.uproxy.colony' + redirectUrl);" + 
+              "document.getElementById('redirectButton').setAttribute('href', 'app://org.uproxy.colony' + path);" + 
               "</script></html>";
 
     if (!(session.getQueryParameterString() == null) &&
-        session.getQueryParameterString().indexOf("token") > -1) {
+        session.getQueryParameterString().indexOf("access_token") > -1) {
       this.code = "http://localhost:" + this.port + session.getUri() +
                   "?" + session.getQueryParameterString();
     }
