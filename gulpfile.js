75050c3fe4364c130fac805fa7236de27c520ce3
Kenny Song <kenny.ysong@gmail.com>
2015-08-13 19:44:54 -0400

    Gulp build_chrome_app and clean now work with copying/deleting the lib modules and are synchronous

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,7 @@ var buffer = require("vinyl-buffer");
 var pkgify = require("pkgify");
 var path = require("path");
 var spawn = require("child_process").spawn;
+var merge = require('merge-stream');
 
 gulp.task("build_provision", function() {
   "use strict";
@@ -51,55 +52,64 @@ gulp.task('copy_forge_min', function(){
 
 gulp.task('copy_chrome_app', function () {
   'use strict';
-  // Copy ssh2-streams and socksv5 node_modules to client/chrome-app/lib
+  // Copy the module ssh2-streams to client/chrome-app/lib
   // We're replacing some files with slightly modified versions
-  gulp.src('./node_modules/ssh2/node_modules/ssh2-streams/node_modules/*')
+  var ss1 = gulp.src('./node_modules/ssh2/node_modules/ssh2-streams/node_modules/**/*')
     .pipe(gulp.dest('./client/chrome-app/lib/ssh2-streams/node_modules/'));
-  gulp.src(['./node_modules/ssh2/node_modules/ssh2-streams/index.js',
+  var ss2 = gulp.src(['./node_modules/ssh2/node_modules/ssh2-streams/index.js',
             './node_modules/ssh2/node_modules/ssh2-streams/package.json'
     ]).pipe(gulp.dest('./client/chrome-app/lib/ssh2-streams/'));
-  gulp.src(['./node_modules/ssh2/node_modules/ssh2-streams/lib/constants.js',
+  var ss3 = gulp.src(['./node_modules/ssh2/node_modules/ssh2-streams/lib/constants.js',
             './node_modules/ssh2/node_modules/ssh2-streams/lib/keyParser.js',
             './node_modules/ssh2/node_modules/ssh2-streams/lib/utils.js',
     ]).pipe(gulp.dest('./client/chrome-app/lib/ssh2-streams/lib/'));
 
-  gulp.src('./node_modules/socksv5/node_modules/*')
+  // Copy the module socksv5 to client/chrome-app/lib, same reason as above
+  var s1 = gulp.src('./node_modules/socksv5/node_modules/**/*')
     .pipe(gulp.dest('./client/chrome-app/lib/socksv5/node_modules/'));
-  gulp.src('./node_modules/socksv5/package.json')
+  var s2 = gulp.src('./node_modules/socksv5/package.json')
     .pipe(gulp.dest('./client/chrome-app/lib/socksv5/'));
-  gulp.src(['./node_modules/socksv5/lib/Agents.js',
+  var s3 = gulp.src(['./node_modules/socksv5/lib/Agents.js',
             './node_modules/socksv5/lib/client.js',
             './node_modules/socksv5/lib/client.parser.js',
             './node_modules/socksv5/lib/constants.js',
             './node_modules/socksv5/lib/server.parser.js',
             './node_modules/socksv5/lib/utils.js',
     ]).pipe(gulp.dest('./client/chrome-app/lib/socksv5/lib/'));
-  gulp.src('./node_modules/socksv5/lib/auth/*')
+  var s4 = gulp.src('./node_modules/socksv5/lib/auth/**/*')
     .pipe(gulp.dest('./client/chrome-app/lib/socksv5/lib/auth/'));
+  // For some reason these two files are not copied by the above rules
+  var s5 = gulp.src('./node_modules/socksv5/node_modules/ipv6/lib/node/bigint.js')
+    .pipe(gulp.dest('./client/chrome-app/lib/socksv5/node_modules/ipv6/lib/node/'));
+  var s6 = gulp.src('./node_modules/socksv5/node_modules/ipv6/lib/browser/sprintf.js')
+    .pipe(gulp.dest('./client/chrome-app/lib/socksv5/node_modules/ipv6/lib/browser/'));
 
   // Copy all relevant files to client/build/chrome-app
-  gulp.src([
+  var c1 = gulp.src([
     './client/chrome-app/background.js',
     './client/chrome-app/index.html',
     './client/chrome-app/manifest.json',
     './client/chrome-app/index.js',
     './client/chrome-app/freedom-ssh.json',
     ]).pipe(gulp.dest('./client/build/chrome-app/'));
-  gulp.src([
+  var c2 = gulp.src([
       './node_modules/forge-min/forge.min.js',
       './node_modules/freedom-for-chrome/freedom-for-chrome.js'
     ]).pipe(gulp.dest('./client/build/chrome-app/js/'));
-  gulp.src('./client/www/css/index.css')
+  var c3 = gulp.src('./client/www/css/index.css')
     .pipe(gulp.dest('./client/build/chrome-app/css/'));
-  gulp.src('./client/www/img/*')
+  var c4 = gulp.src('./client/www/img/*')
     .pipe(gulp.dest('./client/build/chrome-app/img/'));
+
+  // return merge() is necessary so this finishes synchronously
+  return merge(ss1, ss2, ss3, s1, s2, s3, s4, s5, s6, c1, c2, c3, c4);
 });
 
 gulp.task('browserify_chrome_app', function () {
   // Browserify client/chrome-app/freedom-ssh.js  
   // TODO(kennysong): Figure out how to only replace the relevant files with
   // pkgify instead of copying the entire module
-  browserify('./client/chrome-app/freedom-ssh.js', {debug: true})
+  var freedomSsh = browserify('./client/chrome-app/freedom-ssh.js', {debug: true})
     .transform(pkgify, {
       packages: {
         'request': path.relative(__dirname, require.resolve("browser-request")),
@@ -116,7 +126,7 @@ gulp.task('browserify_chrome_app', function () {
     .pipe(gulp.dest('./client/build/chrome-app'));
 
   // Browserify client/chrome-app/index.js  
-  browserify('./client/chrome-app/index.js', {debug: true})
+  var index = browserify('./client/chrome-app/index.js', {debug: true})
     .transform(pkgify, {
       packages: {
         'request': path.relative(__dirname, require.resolve("browser-request"))
@@ -127,6 +137,8 @@ gulp.task('browserify_chrome_app', function () {
     .bundle()
     .pipe(source('index-bundle.js'))
     .pipe(gulp.dest('./client/build/chrome-app'));
+
+  return merge(freedomSsh, index);
 });
 
 gulp.task('build_chrome_app', gulpSequence('copy_chrome_app', 'browserify_chrome_app'));
@@ -195,6 +207,26 @@ gulp.task("build_js", [ "build_provision", "copy_forge_min", "build_chrome_app" 
 gulp.task("run", gulpSequence("build_js", "cordova_emulate"));
 gulp.task("clean", function(cb) { 
   "use strict"; 
+  // Remove the extra node module files in chrome-app/lib
+  fs.remove('./client/chrome-app/lib/ssh2-streams/node_modules/');
+  fs.remove('./client/chrome-app/lib/ssh2-streams/index.js');
+  fs.remove('./client/chrome-app/lib/ssh2-streams/package.json');
+  fs.remove('./client/chrome-app/lib/ssh2-streams/lib/constants.js');
+  fs.remove('./client/chrome-app/lib/ssh2-streams/lib/keyParser.js');
+  fs.remove('./client/chrome-app/lib/ssh2-streams/lib/utils.js');
+  fs.remove('./client/chrome-app/lib/socksv5/node_modules/');
+  fs.remove('./client/chrome-app/lib/socksv5/package.json');
+  fs.remove('./client/chrome-app/lib/socksv5/lib/Agents.js');
+  fs.remove('./client/chrome-app/lib/socksv5/lib/client.js');
+  fs.remove('./client/chrome-app/lib/socksv5/lib/client.parser.js');
+  fs.remove('./client/chrome-app/lib/socksv5/lib/constants.js');
+  fs.remove('./client/chrome-app/lib/socksv5/lib/server.parser.js');
+  fs.remove('./client/chrome-app/lib/socksv5/lib/utils.js');
+  fs.remove('./client/chrome-app/lib/socksv5/lib/auth/');
+  fs.remove('./client/chrome-app/lib/socksv5/node_modules/ipv6/lib/node/');
+  fs.remove('./client/chrome-app/lib/socksv5/node_modules/ipv6/lib/browser/');
+
+  // Remove the build folders
   fs.remove("client/www/build");
   fs.remove("client/build", function() { cb(); });
 });
