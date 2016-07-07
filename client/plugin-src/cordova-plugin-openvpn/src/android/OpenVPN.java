9373e5fa264b7518addeadfaff53e491b6d993ea
alberto lalama <alalama@stanford.edu>
2016-07-07 13:32:56 -0400

    Address review comments, minor improvements

--- client/plugin-src/cordova-plugin-openvpn/src/android/OpenVPN.java
+++ client/plugin-src/cordova-plugin-openvpn/src/android/OpenVPN.java
@@ -15,50 +15,48 @@ import android.content.ComponentName;
 import android.content.ServiceConnection;
 import android.net.VpnService;
 import android.os.IBinder;
-import android.os.RemoteException;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.StringReader;
 
 import de.blinkt.openvpn.VpnProfile;
-import de.blinkt.openvpn.api.GrantPermissionsActivity;
 import de.blinkt.openvpn.core.ConfigParser;
 import de.blinkt.openvpn.core.OpenVPNService;
 import de.blinkt.openvpn.core.ProfileManager;
 import de.blinkt.openvpn.core.VPNLaunchHelper;
 
 public class OpenVPN extends CordovaPlugin {
-  final static String LOG_TAG = "OpenVPNPlugin";
+  final static String LOG_TAG = "OpenVPN";
 
-  private static final int PREPARE_VPN = 0;
+  private static final String ACTION_START_VPN = "startVPN";
+  private static final String ACTION_STOP_VPN = "stopVPN";
+  private static final int REQUEST_CODE_PREPARE_VPN = 0;
+  private static final int RESULT_OK = -1;  // Standard activity result success
 
-  private OpenVPNService mService;
+  private OpenVPNService mOpenVpnService;
 
   @Override
   protected void pluginInitialize() {
     Log.d(LOG_TAG, "OpenVPNPlugin initializing.");
-    // Start service.
-    Intent serviceIntent = new Intent(getBaseContext(), OpenVPNService.class);
-    this.cordova.getActivity().startService(serviceIntent);
-    // Bind service.
-    Intent intent = new Intent(getBaseContext(), OpenVPNService.class);
-    intent.setAction(OpenVPNService.START_SERVICE);
-    this.cordova.getActivity().bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-
-    prepareVPNService();
+    // Bind and start service.
+    Intent bindVpnService = new Intent(getBaseContext(), OpenVPNService.class);
+    bindVpnService.setAction(OpenVPNService.START_SERVICE);
+    getBaseContext().bindService(bindVpnService, mConnection, Context.BIND_AUTO_CREATE);
   }
 
+  // Executes an action. Returns true if the supplied action exists.
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    if (action.equals("startVPN")) {
-      String inlineConfig = args.getString(0);
-      this.startVPN(inlineConfig, callbackContext);
+    if (action.equals(ACTION_START_VPN)) {
+      if (args.length() < 1) {
+        callbackContext.error("Cannot start VPN without config.");
+      } else {
+        String vpnConfig = args.getString(0);
+        this.startVPN(vpnConfig, callbackContext);
+      }
       return true;
-    } else if (action.equals("stopVPN")) {
-      this.stopVPN();
+    } else if (action.equals(ACTION_STOP_VPN)) {
+      stopVPN();
       return true;
     }
     return false;
@@ -69,29 +67,36 @@ public class OpenVPN extends CordovaPlugin {
     public void onServiceConnected(ComponentName className,
                                    IBinder service) {
       OpenVPNService.LocalBinder binder = (OpenVPNService.LocalBinder)service;
-      mService = binder.getService();
+      mOpenVpnService = binder.getService();
     }
 
     @Override
-    public void onServiceDisconnected(ComponentName arg0) {
-        Log.i(LOG_TAG, "VPN disconnected.");
-        mService = null;
+    public void onServiceDisconnected(ComponentName name) {
+      Log.i(LOG_TAG, "VPN disconnected.");
+      getBaseContext().unbindService(mConnection);
+      mOpenVpnService = null;
     }
   };
 
-  private void prepareVPNService() {
-    if (VpnService.prepare(getBaseContext()) == null) {
-      Log.d(LOG_TAG, "Prepared OpenVPN service.");
-    } else {
-      Intent requestPermission = new Intent(getBaseContext(), GrantPermissionsActivity.class);
-      this.cordova.getActivity().startActivityForResult(requestPermission, PREPARE_VPN);
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    if (mOpenVpnService != null) {
+      getBaseContext().unbindService(mConnection);
     }
   }
 
-  private void startVPN(String inlineConfig, CallbackContext callbackContext) {
+  private void startVPN(String vpnConfig, CallbackContext callbackContext) {
+    if (vpnConfig == null || callbackContext == null) {
+      final String msg = "Failed to start VPN service. Missing arguments.";
+      Log.e(LOG_TAG, msg);
+      callbackContext.error(msg);
+      return;
+    }
+
     ConfigParser cp = new ConfigParser();
     try {
-      cp.parseConfig(new StringReader(inlineConfig));
+      cp.parseConfig(new StringReader(vpnConfig));
       VpnProfile vp = cp.convertProfile();
       vp.mName = "OpenVPN";
       vp.mProfileCreator = getBaseContext().getPackageName();
@@ -100,9 +105,9 @@ public class OpenVPN extends CordovaPlugin {
           callbackContext.error(
               getBaseContext().getString(vp.checkProfile(getBaseContext())));
       }
-
       ProfileManager.setTemporaryProfile(vp);
       startVPNWithProfile(vp);
+      callbackContext.success("VPN running");
     } catch (ConfigParser.ConfigParseError e) {
       callbackContext.error(e.getMessage());
     } catch (IOException e) {
@@ -113,8 +118,9 @@ public class OpenVPN extends CordovaPlugin {
   private void startVPNWithProfile(VpnProfile vp) {
     Intent vpnPermissionIntent = VpnService.prepare(getBaseContext());
     int needPassword = vp.needUserPWInput(false);
-
     if(vpnPermissionIntent != null || needPassword != 0){
+      // VPN has not been prepared, this intent will prompt the user for
+      // permissions, and subsequently launch OpenVPN.
       Intent shortVPNIntent = new Intent(Intent.ACTION_MAIN);
       shortVPNIntent.setClass(getBaseContext(), de.blinkt.openvpn.LaunchVPN.class);
       shortVPNIntent.putExtra(de.blinkt.openvpn.LaunchVPN.EXTRA_KEY, vp.getUUIDString());
@@ -128,8 +134,8 @@ public class OpenVPN extends CordovaPlugin {
   }
 
   private void stopVPN() {
-    if (mService != null && mService.getManagement() != null) {
-        mService.getManagement().stopVPN(false);
+    if (mOpenVpnService != null && mOpenVpnService.getManagement() != null) {
+      mOpenVpnService.getManagement().stopVPN(false);
     }
   }
 
